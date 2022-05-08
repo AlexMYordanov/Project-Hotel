6294a421a95d62ff0b925997f04fb1f039a02457
AlexMYordanov <105106682+AlexMYordanov@users.noreply.github.com>
2022-05-08 12:56:36 +0300

    Update Room.cpp

--- Hotel/Room.cpp
+++ Hotel/Room.cpp
@@ -3,13 +3,13 @@ Room::Room()
 {
     number=-1;
     beds=-1;
-    name="None.";
-    note="None.";
+    name="None";
+    note="None";
     start={0,0,0};
     finish={0,0,0};
 }
 
-//fix
+
 std::ostream& operator<<(std::ostream& os, const Room& other)
 {
     os<<"Room: "<<other.number<<" - "<<other.beds;
@@ -17,6 +17,15 @@ std::ostream& operator<<(std::ostream& os, const Room& other)
         os<<" beds";
     else
         os<<" bed";
+    os<<". Guest name: "<<other.name
+      <<". Note: "<<other.note
+      <<". Occupation period: "<<other.start.day<<':'
+                               <<other.start.month<<':'
+                               <<other.start.year<<" - "
+                               <<other.finish.day<<':'
+                               <<other.finish.month<<':'
+                               <<other.finish.year;
+
     return os;
 }
 
@@ -26,15 +35,25 @@ Room& Room::operator=(const Room& other)
         {
         number=other.number;
         beds=other.beds;
-        //fix
+
+        name=other.name;
+        note=other.note;
+
+        start.day=other.start.day;
+        start.month=other.start.month;
+        start.year=other.start.year;
+
+        finish.day=other.finish.day;
+        finish.month=other.finish.month;
+        finish.year=other.finish.year;
     }
 
     return *this;
 }
 void Room::free_room()
 {
-    name="None.";
-    note="None.";
+    name="None";
+    note="None";
     start={0,0,0};
     finish={0,0,0};
 }
@@ -43,9 +62,15 @@ void Room::close(Date& _start,Date& _finish)
     note="In maintenance!";
     start=_start;
     finish=_finish;
-    name="None.";
+    name="None";
 }
-bool Room::operator<(const Room& other)
+bool Room::operator>(const Room& other)
 {
-    return beds<other.beds;
+    return beds>other.beds;
 }
+/*
+ bool Room::NotOccupied( Date& _start, Date& _finish)const
+ {
+     return name == "None" && note == "None" && (this->start>_finish || this->finish<_start);
+ }
+*/
