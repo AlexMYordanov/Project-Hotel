1ff5306d6442cd3bb8c45ec84079d0f6e57ffc64
AlexMYordanov <105106682+AlexMYordanov@users.noreply.github.com>
2022-05-08 12:57:49 +0300

    Update Room.hpp

--- Hotel/Room.hpp
+++ Hotel/Room.hpp
@@ -8,6 +8,30 @@ struct Date
     int year;
     int month;
     int day;
+    bool operator<( Date& other)
+    {
+        if(year<other.year)
+            return true;
+        else if(month<other.month)
+            return true;
+        else if(day<other.day)
+            return true;
+      return false;
+    }
+
+    bool operator>( Date& other )
+    {
+
+        if(year>other.year)
+            return true;
+        else if(month>other.month)
+            return true;
+        else if(day>other.day)
+            return true;
+      return false;
+    }
+
+
 };
 class Room
 {
@@ -23,19 +47,25 @@ class Room
         Room();
         Room(unsigned short _number, unsigned short _beds,String _name,String _note, Date _start, Date _finish): number(_number), beds(_beds), name(_name),note(_note),start(_start),finish(_finish){}
 
-        void set_number(unsigned short _number){number=_number;}
-        void set_beds(unsigned short _beds){beds=_beds;}
+        void set_number(unsigned short _number)
+        {
+            number=_number;
+            beds=number%3+1;
+        }
         void set_name(const String& _name){name=_name;}
         void set_note(const String&  _note){note=_note;}
-        //seteri i geteri
-        const String get_name()const {return name;}
-        const String get_note()const {return note;}
+        void set_start_date(const Date& _start){start=_start;}
+        void set_finish_date(const Date& _finish){finish=_finish;}
+
+
+         String get_name()const {return name;}
+         String get_note()const {return note;}
         const unsigned short get_number()const {return number;}
         const unsigned short get_beds()const {return beds;}
 
         Room& operator=(const Room&);
-        bool operator<(const Room&);
-        bool Occupied()const; // name?
+        bool operator>(const Room&);
+        bool NotOccupied( Date& , Date& )const;
         void free_room();
         void close(Date& , Date& );
         friend std::ostream& operator<<(std::ostream&, const Room&);
