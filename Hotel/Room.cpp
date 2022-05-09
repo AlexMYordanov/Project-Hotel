17846f35036a7ec46261d2c0775970cbc57a891f
AlexMYordanov <105106682+AlexMYordanov@users.noreply.github.com>
2022-05-09 21:13:37 +0300

    Update Room.cpp

--- Hotel/Room.cpp
+++ Hotel/Room.cpp
@@ -68,9 +68,61 @@ bool Room::operator>(const Room& other)
 {
     return beds>other.beds;
 }
-/*
- bool Room::NotOccupied( Date& _start, Date& _finish)const
+
+ bool Room::NotOccupied(const Date& _start, const Date& _finish)const
  {
-     return name == "None" && note == "None" && (this->start>_finish || this->finish<_start);
+     return name == "None" && note == "None" && (start>_finish || finish<_start);
  }
-*/
+
+int Room::Used(unsigned short _number,const Date& _start,const Date& _finish )const
+{
+    Date none;
+
+        if(this->get_number()==_number )
+        {
+            if(this->get_start()== none || this->get_start()>_finish || this->get_finish()<_start)
+                return 0;
+            else
+            {
+                if(this->get_start()<_start && this->get_finish()<_finish)
+                    return Difference(_start,this->get_finish());
+
+               else if(this->get_start()<_start && this->get_finish()>_finish)
+                    return Difference(_start,_finish);
+
+                else if(this->get_start()>_start && this->get_finish()<_finish)
+                    return Difference(this->get_start(),this->get_finish());
+
+                else if(this->get_start()>_start && this->get_finish()>_finish)
+                    return Difference(this->get_start(),_finish);
+            }
+        }
+
+
+}
+int Room::Difference(const Date& _start,const Date& _finish) const
+{
+
+    const int month_days[12]={31,28,31,30,31,30,31,31,30,31,30,31};
+   long int days_start = (_start.year * 365);
+
+   for (int i = 0; i < _start.month - 1; i++)
+
+        days_start += month_days[i];
+
+
+   days_start += _start.day;
+
+
+
+   long int days_finish= (_finish.year * 365);
+    for (int i = 0; i < _finish.month - 1; i++)
+        days_finish += month_days[i];
+
+   days_finish += _finish.day;
+
+
+   int days=days_finish -days_start;
+
+   return  (_finish.year==2024 && _finish.month>=2) ? days + 1 : days ;
+}
