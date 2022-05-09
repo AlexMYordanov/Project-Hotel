1e0013f27f40dd4c98da5ac3d36c68a37ebb3362
AlexMYordanov <105106682+AlexMYordanov@users.noreply.github.com>
2022-05-09 21:14:22 +0300

    Update Room.hpp

--- Hotel/Room.hpp
+++ Hotel/Room.hpp
@@ -3,36 +3,8 @@
 
 #include <iostream>
 #include "String.hpp"
-struct Date
-{
-    int year;
-    int month;
-    int day;
-    bool operator<( Date& other)
-    {
-        if(year<other.year)
-            return true;
-        else if(month<other.month)
-            return true;
-        else if(day<other.day)
-            return true;
-      return false;
-    }
+#include "Date.hpp"
 
-    bool operator>( Date& other )
-    {
-
-        if(year>other.year)
-            return true;
-        else if(month>other.month)
-            return true;
-        else if(day>other.day)
-            return true;
-      return false;
-    }
-
-
-};
 class Room
 {
     private:
@@ -52,6 +24,7 @@ class Room
             number=_number;
             beds=number%3+1;
         }
+
         void set_name(const String& _name){name=_name;}
         void set_note(const String&  _note){note=_note;}
         void set_start_date(const Date& _start){start=_start;}
@@ -60,16 +33,20 @@ class Room
 
          String get_name()const {return name;}
          String get_note()const {return note;}
+         Date get_start()const {return start;}
+         Date get_finish()const {return finish;}
         const unsigned short get_number()const {return number;}
         const unsigned short get_beds()const {return beds;}
 
         Room& operator=(const Room&);
         bool operator>(const Room&);
-        bool NotOccupied( Date& , Date& )const;
+        bool NotOccupied(const Date& ,const Date& )const;
         void free_room();
         void close(Date& , Date& );
+        int Used(const unsigned short,const Date&,const Date&)const;
         friend std::ostream& operator<<(std::ostream&, const Room&);
+        int Difference(const Date& ,const Date&)const;
 
 };
-
+ 
 #endif // ROOM_HPP_INCLUDED
