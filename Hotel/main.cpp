86877ee3994503ec5a31cc87193991663ee0aaac
AlexMYordanov <105106682+AlexMYordanov@users.noreply.github.com>
2022-05-09 21:11:09 +0300

    Update main.cpp

--- Hotel/main.cpp
+++ Hotel/main.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include "Hotel.hpp"
-#include "String.hpp"
 
 const int max_room=100;
 
-using namespace std;
 
    int main()
 {
 
     Hotel h;
     h.reserve(max_room);
+    /*
     std::ofstream file("database.txt");
     if(!file.is_open())
     {
@@ -20,6 +19,7 @@ using namespace std;
     }
     file<<h<<'\n';
     file.close();
+    */
 
     std::cout<<"Choose one of the following:\n";
     std::cout<<"1. Client registration.\n";
@@ -28,7 +28,8 @@ using namespace std;
     std::cout<<"4. Rooms status.\n";
     std::cout<<"5. Room filter.\n";
     std::cout<<"6. Indicate maintenance.\n";
-    std::cout<<"7. Exit.\n";
+    std::cout<<"7. List of all rooms and current status.\n";
+    std::cout<<"8. Exit.\n";
 
     unsigned short choice;
     unsigned short room_number;
@@ -38,12 +39,7 @@ using namespace std;
         do{
           std::cout<<"\nEnter a command:";
           std::cin>>choice;
-          }while(choice<1 || choice>7 );
-
-
-
-        unsigned short room_number;
-        unsigned short beds;
+          }while(choice<1 || choice>8 );
 
 
         switch(choice)
@@ -53,26 +49,35 @@ using namespace std;
                     std::cin>>room_number;
 
                     Date one, two;
-                    std::cout<<"Please enter start of reservation: ";
-                    std::cin>>one.year>>one.month>>one.day;
-                    std::cout<<"Please enter end of reservation: ";
-                    std::cin>>two.year>>two.month>>two.day;
-                    std::cout<<"Please enter guest name: ";
 
+                    do{
+                    std::cout<<"Please enter start of reservation: ";
+                    std::cin>>one.day>>one.month>>one.year;
+                    std::cout<<"Please enter end of reservation: ";
+                    std::cin>>two.day>>two.month>>two.year;
+
+                    }while(!one.isValidDate() ||  !two.isValidDate());
+
+                    std::cout<<"Please enter guest name: ";
                     String name;
                     std::cin.ignore();
                     name.get_line();
-
-                    std::cout<<"Please enter a note: ";
-                    String note;
-                    note.get_line();
 
                     h[room_number-1].set_name(name);
                     h[room_number-1].set_start_date(one);
                     h[room_number-1].set_finish_date(two);
                     }break;
 
-            case 2:  {int cnt=0;
+            case 2:  {
+                    Date one, two;
+
+                    do{
+                    std::cout<<"Please enter start of period: ";
+                    std::cin>>one.day>>one.month>>one.year;
+                    std::cout<<"Please enter end of period: ";
+                    std::cin>>two.day>>two.month>>two.year;
+                    }while(!one.isValidDate() ||  !two.isValidDate());
+                     int cnt=0;
                      int* available=h.AvailableRooms(cnt);
                      for(int i=0;i<cnt;++i)
                         std::cout<<available[i]<<' ';
@@ -83,29 +88,60 @@ using namespace std;
                     std::cin>>room_number;
                     h.FreeRoom(room_number);}break;
 
-            case 4: break;
+            case 4: {
+                    Date one, two;
+
+                    do{
+                    std::cout<<"Please enter start of period: ";
+                    std::cin>>one.day>>one.month>>one.year;
+                    std::cout<<"Please enter end of period: ";
+                    std::cin>>two.day>>two.month>>two.year;
+                    }while(!one.isValidDate() ||  !two.isValidDate());
+
+                     char title[18]={'\0'};
+                     
+                    std::ofstream file("status.txt");
+                        if(!file.is_open())
+                        {
+                            std::cout<<"error";
+                            return 1;
+                        }
+                        for(int i=0;i<max_room;++i)
+                                file<<"Room "<<h[i].get_number()<<":  "
+                                   <<h[i].Used(h[i].get_number(),one,two)<<" days\n";
+                        file.close();}break;
 
             case 5: {std::cout<<"Please enter the desired number of beds: ";
                     std::cin>>beds;
                     Date one, two;
+
+                     do{
                     std::cout<<"Please enter start of period: ";
-                    std::cin>>one.year>>one.month>>one.day;
+                    std::cin>>one.day>>one.month>>one.year;
                     std::cout<<"Please enter end of period: ";
-                    std::cin>>two.year>>two.month>>two.day;
+                    std::cin>>two.day>>two.month>>two.year;
+                    }while(!one.isValidDate() ||  !two.isValidDate());
+
+                    std::cout<<"Number of a suitable room: ";
                     std::cout<<h.SuitableRoom(beds,one,two);}break;
 
             case 6:{ std::cout<<"Please enter a room number: ";
                     std::cin>>room_number;
                     Date one, two;
+                     do{
                     std::cout<<"Please enter start of maintenance: ";
-                    std::cin>>one.year>>one.month>>one.day;
+                    std::cin>>one.day>>one.month>>one.year;
                     std::cout<<"Please enter end of maintenance: ";
-                    std::cin>>two.year>>two.month>>two.day;
+                    std::cin>>two.day>>two.month>>two.year;
+                    }while(!one.isValidDate() ||  !two.isValidDate());
+
                     h.CloseRoom(room_number,one,two);}break;
+
+           case 7:{std::cout<<h;}break;
         }
 
-    }while(choice!=7);
-     std::cout<<h;
+    }while(choice!=8);
+
 
     return 0;
 }
