8d1ec3cd1eafde1399c64df9c71a822cb1a2c6f3
AlexMYordanov <105106682+AlexMYordanov@users.noreply.github.com>
2022-05-19 16:26:24 +0300

    Update Date.hpp

--- Hotel/Date.hpp
+++ Hotel/Date.hpp
@@ -1,26 +1,20 @@
 #ifndef DATE_HPP_INCLUDED
 #define DATE_HPP_INCLUDED
 
-struct Date
+class Date
 {
+    public:
     int year;
     int month;
     int day;
-    // void set_year(const int _year){year=_year;}
-    //  void set_month(const int _month){month=_month;}
-    //   void set_day(const int _day){day=_day;}
 
-
+    Date():year(0),month(0), day(0){};
+    void clearDate();
     bool operator<( const Date& )const;
     bool operator>( const Date&  )const;
+    bool operator>=( const Date&  )const;
     bool operator==( const Date& )const;
-    int get_day()const;
     bool isValidDate()const;
-    //bool LeapYearDay(const Date&,const Date&) const;
-    //int Difference(Date& , Date&);
-
-
-
 };
 
 #endif // DATE_HPP_INCLUDED
